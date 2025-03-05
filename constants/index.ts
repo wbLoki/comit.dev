327bdd8326c7b41359f8517c5af60e47d316e8a8
wbLoki <wailbouymaj@gmail.com>
2025-03-05 19:53:14 +0000

    update footer

--- constants/index.ts
+++ constants/index.ts
@@ -96,74 +96,24 @@ export const stats = [
     },
 ];
 
-export const footerLinks = [
-    {
-        id: 'footerLinks-1',
-        title: 'Useful Links',
-        links: [
-            {
-                name: 'Content',
-                link: 'https://www.hoobank.com/content/',
-            },
-            {
-                name: 'How it Works',
-                link: 'https://www.hoobank.com/how-it-works/',
-            },
-            {
-                name: 'Create',
-                link: 'https://www.hoobank.com/create/',
-            },
-            {
-                name: 'Explore',
-                link: 'https://www.hoobank.com/explore/',
-            },
-            {
-                name: 'Terms & Services',
-                link: 'https://www.hoobank.com/terms-and-services/',
-            },
-        ],
-    },
-    {
-        id: 'footerLinks-2',
-        title: 'Community',
-        links: [
-            {
-                name: 'Help Center',
-                link: 'https://www.hoobank.com/help-center/',
-            },
-            {
-                name: 'Partners',
-                link: 'https://www.hoobank.com/partners/',
-            },
-            {
-                name: 'Suggestions',
-                link: 'https://www.hoobank.com/suggestions/',
-            },
-            {
-                name: 'Blog',
-                link: 'https://www.hoobank.com/blog/',
-            },
-            {
-                name: 'Newsletters',
-                link: 'https://www.hoobank.com/newsletters/',
-            },
-        ],
-    },
-    {
-        id: 'footerLinks-3',
-        title: 'Partner',
-        links: [
-            {
-                name: 'Our Partner',
-                link: 'https://www.hoobank.com/our-partner/',
-            },
-            {
-                name: 'Become a Partner',
-                link: 'https://www.hoobank.com/become-a-partner/',
-            },
-        ],
-    },
-];
+export const footerLink = {
+    id: 'footerLinks-1',
+    title: 'Useful Links',
+    links: [
+        {
+            name: 'How it Works',
+            link: 'https://www.hoobank.com/how-it-works/',
+        },
+        {
+            name: 'Help',
+            link: 'https://www.hoobank.com/help-center/',
+        },
+        {
+            name: 'Terms & Services',
+            link: 'https://www.hoobank.com/terms-and-services/',
+        },
+    ],
+};
 
 export const socialMedia = [
     {
