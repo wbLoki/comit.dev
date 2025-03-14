3fe1a4576e359df2ba022957d05d04d5b5840cfb
Wail Bouymaj <wailbouymaj@gmail.com>
2025-03-14 00:26:11 +0000

    refactor(docs): simplify layout component by removing unused breadcrumb imports

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,20 +1,10 @@
 'use client';
 import { AppSidebar } from '@components/app-sidebar';
 import {
-    Breadcrumb,
-    BreadcrumbList,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    BreadcrumbSeparator,
-    BreadcrumbPage,
-} from '@components/ui/breadcrumb';
-import { Separator } from '@components/ui/separator';
-import {
     SidebarInset,
     SidebarProvider,
     SidebarTrigger,
 } from '@components/ui/sidebar';
-import Link from 'next/link';
 
 export default function DocumentationLayout({
     children,
@@ -33,20 +23,6 @@ export default function DocumentationLayout({
             <SidebarInset>
                 <header className='flex h-16 shrink-0 items-center gap-2 px-4'>
                     <SidebarTrigger className='-ml-1' />
-                    <Separator orientation='vertical' className='mr-2 h-4' />
-                    <Breadcrumb>
-                        <BreadcrumbList>
-                            <BreadcrumbItem className='hidden md:block'>
-                                <BreadcrumbLink href='#'>
-                                    Building Your Application
-                                </BreadcrumbLink>
-                            </BreadcrumbItem>
-                            <BreadcrumbSeparator className='hidden md:block' />
-                            <BreadcrumbItem>
-                                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                            </BreadcrumbItem>
-                        </BreadcrumbList>
-                    </Breadcrumb>
                 </header>
                 <div className='flex flex-1 flex-col gap-4 p-4 md:px-12 lg:px-16'>
                     {children}
