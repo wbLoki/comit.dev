90501369d49fbffa0259e1038dad6b9e5061aa84
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-03-16 16:13:32 +0100

    feat(CodeSnippet): implement copy to clipboard functionality for code samples

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,24 +1,21 @@
-import Image, { ImageProps } from '@node_modules/next/image';
 import type { MDXComponents } from 'mdx/types';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         // Allows customizing built-in components, e.g. to add styling.
         h1: ({ children }) => (
-            <h1 style={{ color: 'red', fontSize: '44px' }}>{children}</h1>
+            <h1 style={{ color: 'white', fontSize: '44px' }}>{children}</h1>
         ),
         h2: ({ children }) => (
-            <h2 style={{ color: 'red', fontSize: '32px' }}>{children}</h2>
+            <h2 style={{ color: 'white', fontSize: '32px' }}>{children}</h2>
         ),
         h3: ({ children }) => (
-            <h3 style={{ color: 'red', fontSize: '24px' }}>{children}</h3>
+            <h3 style={{ color: 'white', fontSize: '24px' }}>{children}</h3>
         ),
-        img: (props) => (
-            <Image
-                sizes='100vw'
-                style={{ width: '100%', height: 'auto' }}
-                {...(props as ImageProps)}
-            />
+        ul: ({ children }) => (
+            <ul style={{ listStyleType: 'disc', marginLeft: '2rem' }}>
+                {children}
+            </ul>
         ),
         ...components,
     };
