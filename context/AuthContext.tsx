061b7a2870e086e125e6c0049337e838a98585f5
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-03-22 11:11:59 +0100

    feat(auth): add user registration functionality to AuthContext and AuthProvider

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,12 +1,18 @@
-import { createContext } from 'react';
+import { createContext } from "react";
 
 export interface AuthState {
-    user: string | null;
-    token: string | null;
-    login: (username: string, password: string) => Promise<void>;
-    logout: () => void;
-    isAuthenticated: boolean;
-    isLoading: boolean;
+  user: string | null;
+  token: string | null;
+  login: (username: string, password: string) => Promise<void>;
+  logout: () => void;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  register: (
+    username: string,
+    password: string,
+    email: string,
+    confirmPassword: string
+  ) => Promise<void>;
 }
 
 export const AuthContext = createContext<AuthState | undefined>(undefined);
