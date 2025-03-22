aa801cf661b69697a9483100feeb46f98d562353
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-03-22 11:10:34 +0100

    feat(RegisterForm): add registration form component with validation and error handling

---
+++ components/RegisterForm.tsx
@@ -0,0 +1,174 @@
+"use client";
+
+import React from "react";
+import { Button, Input, Checkbox, Link, Divider, Image } from "@heroui/react";
+import { Icon } from "@iconify/react";
+import ErrorMessage from "./ErrorMessage";
+import { useAuth } from "@hooks/useAuth";
+
+export default function RegisterForm() {
+  const [isVisible, setIsVisible] = React.useState(false);
+  const [isConfirmVisible, setIsConfirmVisible] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const { register } = useAuth();
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+  const toggleConfirmVisibility = () => setIsConfirmVisible(!isConfirmVisible);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const data = {
+      username: formData.get("username") as string,
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
+      confirmPassword: formData.get("confirmPassword") as string,
+    };
+
+    if (data.password !== data.confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    if (data.password.length < 8) {
+      setError("Password must be at least 8 characters long");
+      return;
+    }
+    if (data.username.length < 5) {
+      setError("Username must be at least 5 characters long");
+      return;
+    }
+
+    try {
+      await register(
+        data.username,
+        data.password,
+        data.email,
+        data.confirmPassword
+      );
+    } catch (err: any) {
+      setError(err.message || "Invalid username or password");
+    }
+  };
+
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-4 rounded-large">
+        <div className="flex flex-col items-center pb-6">
+          <Image src="assets/icons/logo-dark.png" alt="logo" />
+        </div>
+        <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+          {error && <ErrorMessage data={{ message: error }} />}
+          <div className="flex flex-col">
+            <Input
+              isRequired
+              classNames={{
+                base: "-mb-[2px]",
+                inputWrapper:
+                  "rounded-b-none data-[hover=true]:z-10 group-data-[focus-visible=true]:z-10",
+              }}
+              label="Username"
+              name="username"
+              placeholder="Enter your username"
+              type="text"
+              variant="bordered"
+            />
+            <Input
+              isRequired
+              classNames={{
+                base: "-mb-[2px]",
+                inputWrapper:
+                  "rounded-none data-[hover=true]:z-10 group-data-[focus-visible=true]:z-10",
+              }}
+              label="Email Address"
+              name="email"
+              placeholder="Enter your email"
+              type="email"
+              variant="bordered"
+            />
+            <Input
+              isRequired
+              classNames={{
+                base: "-mb-[2px]",
+                inputWrapper:
+                  "rounded-none data-[hover=true]:z-10 group-data-[focus-visible=true]:z-10",
+              }}
+              endContent={
+                <button type="button" onClick={toggleVisibility}>
+                  {isVisible ? (
+                    <Icon
+                      className="pointer-events-none text-2xl text-default-400"
+                      icon="solar:eye-closed-linear"
+                    />
+                  ) : (
+                    <Icon
+                      className="pointer-events-none text-2xl text-default-400"
+                      icon="solar:eye-bold"
+                    />
+                  )}
+                </button>
+              }
+              label="Password"
+              name="password"
+              placeholder="Enter your password"
+              type={isVisible ? "text" : "password"}
+              variant="bordered"
+            />
+            <Input
+              isRequired
+              classNames={{
+                inputWrapper: "rounded-t-none",
+              }}
+              endContent={
+                <button type="button" onClick={toggleConfirmVisibility}>
+                  {isConfirmVisible ? (
+                    <Icon
+                      className="pointer-events-none text-2xl text-default-400"
+                      icon="solar:eye-closed-linear"
+                    />
+                  ) : (
+                    <Icon
+                      className="pointer-events-none text-2xl text-default-400"
+                      icon="solar:eye-bold"
+                    />
+                  )}
+                </button>
+              }
+              label="Confirm Password"
+              name="confirmPassword"
+              placeholder="Confirm your password"
+              type={isConfirmVisible ? "text" : "password"}
+              variant="bordered"
+            />
+          </div>
+          <Checkbox isRequired className="py-4 text-default" size="sm">
+            I agree with the&nbsp;
+            <Link className="relative z-[1]" href="#" size="sm">
+              Terms
+            </Link>
+            &nbsp; and&nbsp;
+            <Link className="relative z-[1]" href="#" size="sm">
+              Privacy Policy
+            </Link>
+          </Checkbox>
+          <Button color="primary" type="submit">
+            Sign Up
+          </Button>
+        </form>
+        <div className="flex items-center gap-4 py-2">
+          <Divider className="flex-1" />
+          <p className="shrink-0 text-tiny text-default-500">OR</p>
+          <Divider className="flex-1" />
+        </div>
+        <p className="text-center text-small">
+          Already have an account?&nbsp;
+          <Link href="#" className="text-default" size="sm">
+            Log In
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
