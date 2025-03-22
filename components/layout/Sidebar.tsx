894cf1effbbe3466ead970a76ff62a929b525a15
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-03-22 22:40:44 +0100

    fix: replace deprecated image import with next/image for better optimization

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import { Button, Image, Link } from '@heroui/react';
+import { Button, Link } from '@heroui/react';
 import { FC, ReactNode } from '@node_modules/@types/react';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
 import { Icon } from '@iconify/react';
 import { useAuth } from '@hooks/useAuth';
+import Image from 'next/image';
 
 interface SidebarProps {
     children: ReactNode;
@@ -83,6 +84,9 @@ const Sidebar: FC<SidebarProps> = ({ children }) => {
                             <Image
                                 src='/assets/icons/logo-mobile-dark.png'
                                 className='h-8 w-full'
+                                alt='comit.dev'
+                                width={32}
+                                height={32}
                             />
                             <span>Comit.dev</span>
                         </div>
