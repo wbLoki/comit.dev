d97b6a165ac86c580e0b614fab8a3e8f8354a953
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-03-22 23:13:55 +0100

    feat(layout): add Google Analytics integration to RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,36 @@
-import '../styles/globals.css';
-import Providers from './Providers';
+import Script from "next/script";
+import "../styles/globals.css";
+import Providers from "./Providers";
 export const metadata = {
-    title: 'Comit',
+  title: "Comit",
 
-    description:
-        'An AI-powered tool for generating meaningful commit messages and providing coding assistance',
+  description:
+    "An AI-powered tool for generating meaningful commit messages and providing coding assistance",
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <html className='dark' lang='en'>
-            <body className='dark:bg-background'>
-                <Providers>{children}</Providers>
-            </body>
-        </html>
-    );
+  return (
+    <html className="dark" lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-SXB4QC7DBJ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-SXB4QC7DBJ');
+          `}
+        </Script>
+      </head>
+      <body className="dark:bg-background">
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
