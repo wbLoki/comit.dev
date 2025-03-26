322db09afb636b4439b0bdf3622ef8fac4d3bf62
Wail Bouymaj <wailbouymaj@gmail.com>
2025-03-26 22:39:09 +0000

    fix: redirect on login to dashboard

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -1,99 +1,113 @@
-"use client";
+'use client';
 
-import { useState, useEffect } from "react";
-import { AuthContext } from "./AuthContext";
-import { useRouter } from "next/navigation";
-import Cookies from "js-cookie";
-import api from "@lib/api";
+import { useState, useEffect } from 'react';
+import { AuthContext } from './AuthContext';
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
+import api from '@lib/api';
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const [user, setUser] = useState<string | null>(null);
-  const [token, setToken] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const router = useRouter();
+    const [user, setUser] = useState<string | null>(null);
+    const [token, setToken] = useState<string | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
 
-  useEffect(() => {
-    const fetchTokens = async () => {
-      try {
-        const response = await api.get("users/me/");
-        if (response.status !== 200){
-          return
+    useEffect(() => {
+        const fetchTokens = async () => {
+            try {
+                const response = await api.get('users/me/');
+                if (response.status !== 200) {
+                    return;
+                }
+                setUser(response.data.username);
+                setToken(response.data.tokens[0].token);
+            } catch (error) {
+                console.error('Error fetching token:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchTokens();
+    }, []);
+
+    const register = async (
+        username: string,
+        password: string,
+        email: string,
+        confirmPassword: string
+    ) => {
+        try {
+            const response = await api.post('auth/register', {
+                username,
+                password,
+                email,
+                confirmPassword,
+            });
+            if (response.status !== 200) {
+                if (response.data.detail) {
+                    throw new Error(response.data.detail);
+                }
+                throw new Error('Registration failed');
+            }
+            router.push('/login');
+        } catch (error: any) {
+            if (error.status === 400) {
+                throw (
+                    error.response?.data?.detail ||
+                    'Invalid username or password'
+                );
+            }
+            throw error;
         }
-        setUser(response.data.username);
-        setToken(response.data.tokens[0].token);
-      } catch (error) {
-        console.error("Error fetching token:", error);
-      } finally {
-        setLoading(false);
-      }
     };
 
-    fetchTokens();
-  }, []);
+    const login = async (username: string, password: string) => {
+        try {
+            const response = await api.post('auth/login', {
+                username,
+                password,
+            });
 
-  const register = async (username: string, password: string, email: string, confirmPassword: string) => {
-    try {
-      const response = await api.post("auth/register", {
-        username,
-        password,
-        email,
-        confirmPassword,
-      });
-      if (response.status !== 200) {
-        if (response.data.detail){
-          throw new Error(response.data.detail);
+            Cookies.set('access_token', response.data.access_token);
+            Cookies.set('username', username);
+
+            setUser(username);
+            router.push('/dashboard');
+        } catch (error: any) {
+            console.error(
+                'Login failed:',
+                error.response?.data?.detail || 'Error'
+            );
+            if (error.status === 401) {
+                throw (
+                    error.response?.data?.detail ||
+                    'Invalid username or password'
+                );
+            }
         }
-        throw new Error("Registration failed");
-      }
-      router.push("/login");
-    } catch (error: any) {
-      if (error.status === 400) {
-        throw error.response?.data?.detail || "Invalid username or password";
-      }
-      throw error;
-    }
-  };
+    };
 
-  const login = async (username: string, password: string) => {
-    try {
-      const response = await api.post("auth/login", {
-        username,
-        password,
-      });
+    const logout = () => {
+        Cookies.remove('access_token');
+        Cookies.remove('username');
+        setUser(null);
+        router.push('/');
+    };
 
-      Cookies.set("access_token", response.data.access_token);
-      Cookies.set("username", username);
-
-      setUser(username);
-      router.push("/");
-    } catch (error: any) {
-      console.error("Login failed:", error.response?.data?.detail || "Error");
-      if (error.status === 401) {
-        throw error.response?.data?.detail || "Invalid username or password";
-      }
-    }
-  };
-
-  const logout = () => {
-    Cookies.remove("access_token");
-    Cookies.remove("username");
-    setUser(null);
-    router.push("/");
-  };
-
-  return (
-    <AuthContext.Provider
-      value={{
-        user,
-        token,
-        login,
-        logout,
-        isAuthenticated: !!user,
-        isLoading: loading,
-        register,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                token,
+                login,
+                logout,
+                isAuthenticated: !!user,
+                isLoading: loading,
+                register,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
