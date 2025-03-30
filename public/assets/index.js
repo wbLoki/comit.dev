c3957774c9cdffe13425e77241153754c86cd25a
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-03-30 13:18:35 +0200

    feat(assets): add new GIF for commit visualization

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,33 +1,37 @@
-import Ai from './icons/Ai.svg';
-import arrowUp from './arrow-up.svg';
-import branch from './icons/Branch.svg';
-import close from './close.svg';
-import commit from './icons/Commit.svg';
-import logo from './icons/logo.png';
-import darkLogo from './icons/logo-dark.png';
-import mobileLogo from './icons/logo-mobile-dark.png';
-import terminal from './terminal.png';
-import menu from './menu.svg';
-import facebook from './facebook.svg';
-import instagram from './instagram.svg';
-import linkedin from './linkedin.svg';
-import twitter from './twitter.svg';
-import github from './github.svg';
+import Ai from "./icons/Ai.svg";
+import arrowUp from "./arrow-up.svg";
+import branch from "./icons/Branch.svg";
+import close from "./close.svg";
+import commit from "./icons/Commit.svg";
+import logo from "./icons/logo.png";
+import darkLogo from "./icons/logo-dark.png";
+import mobileLogo from "./icons/logo-mobile-dark.png";
+import terminal from "./terminal.png";
+import menu from "./menu.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import github from "./github.svg";
+import comit from "./comit.gif";
+import newComit from "./new_comit.gif";
 
 export {
-    Ai,
-    arrowUp,
-    branch,
-    close,
-    commit,
-    facebook,
-    github,
-    instagram,
-    linkedin,
-    logo,
-    darkLogo,
-    mobileLogo,
-    menu,
-    terminal,
-    twitter,
+  Ai,
+  arrowUp,
+  branch,
+  close,
+  commit,
+  facebook,
+  github,
+  instagram,
+  linkedin,
+  logo,
+  darkLogo,
+  mobileLogo,
+  menu,
+  terminal,
+  twitter,
+  comit,
+  newComit,
 };
